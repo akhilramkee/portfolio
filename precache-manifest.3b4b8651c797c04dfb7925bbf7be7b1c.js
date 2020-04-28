4fb67df9677cccb03a446f0a518ed3211d162357
akhilramkee <akhilramkee123@gmail.com>
2020-04-28 20:36:13 +0530

    Updates

---
+++ precache-manifest.3b4b8651c797c04dfb7925bbf7be7b1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ef31581a9a3099843eea7da40a272ca",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3fc44e9289f49c8c7be4",
+    "url": "/portfolio/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "9e0e1d5e2eaf51248c57",
+    "url": "/portfolio/static/css/main.4f11c7ae.chunk.css"
+  },
+  {
+    "revision": "3fc44e9289f49c8c7be4",
+    "url": "/portfolio/static/js/2.0d713150.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.0d713150.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e0e1d5e2eaf51248c57",
+    "url": "/portfolio/static/js/main.250e592b.chunk.js"
+  },
+  {
+    "revision": "6c1d3ad26a766dc494c1",
+    "url": "/portfolio/static/js/runtime-main.d597d31e.js"
+  }
+]);
