4b4317c80aa18a8fd0ee6105de3aa1a2c250c681
akhilramkee <akhilramkee123@gmail.com>
2020-05-11 22:45:11 +0530

    Added Tranparent Layer

--- src/components/repeatables/card.js
+++ src/components/repeatables/card.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import Rating from 'react-rating';
+import Avatar from '@material-ui/core/Avatar';
+import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles({
   root: {
     maxWidth: 300,
+    background:'transparent',
+    marginBottom:'10%'
   },
 });
 
@@ -17,22 +20,23 @@ export default function RatingCard(props) {
 
   return (
     <Card className={classes.root} elevation={10}>
-        <CardMedia
-          component="img"
-          alt={props.title}
-          height="140"
-          image={props.image}
-          title={props.title}
-        />
-    <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            {props.title}
-          </Typography>
+    
+    <CardContent style={{marginTop:'5%'}}>
           <Typography variant="body2" color="textPrimary" component="p">
-            <Rating 
-                initialRating = {props.rating}
-                readonly
-            />
+            <Grid container
+                  direction="row"
+                  alignItems="center"
+                  justify="center"
+                  spacing={3}
+            >
+            <Grid items xs={2} spacing={5}>
+                <Avatar alt={props.title} src={props.image} />
+            </Grid>
+              <Rating 
+                  initialRating = {props.rating}
+                  readonly
+              />
+            </Grid>
           </Typography>
         </CardContent>
     </Card>
--- src/components/skills.js
+++ src/components/skills.js
@@ -45,18 +45,18 @@ export default function Skills(){
                       direction="row"
                       justify="center"
                       alignItems="center"
-                      style={{marginTop:'5%'}}
+                      style={{marginTop:'2%'}}
                 >
-                    <Grid item md={3}>
+                    <Grid item md={3} xs={8}>
                         <RatingCard image="https://www.inovex.de/blog/wp-content/uploads/2022/01/one-year-of-react-native.png" title="React" rating={4} bk="linear-gradient(to right, #d38312, #a83279)" />
                     </Grid>
-                    <Grid item md={3}>
+                    <Grid item md={3} xs={8}>
                         <RatingCard image="https://colorlib.com/wp/wp-content/uploads/sites/2/nodejs-frameworks.png" title="NodeJS" rating={3} />
                     </Grid>
-                    <Grid item md={3}>
+                    <Grid item md={3} xs={8}>
                         <RatingCard image="https://pluralsight.imgix.net/paths/python-7be70baaac.png" title="Python" rating={3} />
                     </Grid>
-                    <Grid item md={3}>
+                    <Grid item md={3} xs={8}>
                         <RatingCard image="https://pbs.twimg.com/profile_images/954020529391902720/jW4dnFtA_400x400.jpg" title="Java" rating={3} />
                     </Grid>
                 </Grid>
